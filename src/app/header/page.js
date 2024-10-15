355796ac3ffa7bfc1137ab145790b46da30d4d75
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-10-14 19:14:47 -0600

    Inicio

---
+++ src/app/header/page.js
@@ -0,0 +1,8 @@
+import logo from "../../../starter-code/assets/logo.svg";
+export default function Header() {
+    return (
+        <header>
+            <img src={logo} alt="logo" />
+        </header>
+    )
+}
