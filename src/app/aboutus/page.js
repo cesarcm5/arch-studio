30ad9fa94593798f4112f365c1ce076285c2201a
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-11-07 11:39:08 -0600

    about fixed layout

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -22,7 +22,7 @@ const crew = [
         rol: "Senior Architect",
     }
 ]
-export default function aboutus() {
+export default function Aboutus() {
     return (
         <main className="my-8 mx-12 px-12">
             <Header/>
@@ -30,14 +30,14 @@ export default function aboutus() {
                 <div className="absolute">
                     <img src="assets/about/desktop/image-hero.jpg"/>
                 </div>
-                <div className="mt-12">
+                <div className="">
                     <div id="about" className="text-end relative font-bold text-gray-300">
-                        <p>About</p>
+                        <p id="about1">About</p>
                     </div>
-                    <div className="relative bg-white flex align-center">
+                    <div id="team" className="w-3/5 relative bg-white flex align-center">
                         <div className="">
-                            <p className="font-bold text-4xl">Your team of professionals</p>
-                            <p>Our small team of world-class professionals will work with you every step of the way.<br/> Strong relationships are at the core of everything we do.<br/> This extends to the relationship our projects have with their surroundings.</p>
+                            <p className="font-bold text-5xl mb-4 p-12">Your team of <br/> professionals</p>
+                            <p className="">Our small team of world-class professionals will work with you every step of the way.Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.</p>
                         </div>
                     </div>
                 </div>
