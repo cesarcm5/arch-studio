557db76125f4d68ecf042a38601375968209ea6c
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-11-13 14:00:34 -0600

    Contact page advance

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -27,17 +27,19 @@ export default function Contact() {
                     </div>
                 </div>
             </div>
-            <div className="p-8">
-                <h2 className="text-3xl font-bold">Contact Details</h2>
-                <div className="flex justify-between mt-6">
+            <div className="p-8 flex mt-12 mb-12">
+                <div className='p-5'>
+                    <p className='font-bold text-6xl'>Contact <br/> Details</p>
+                </div>
+                <div id="contact" className="flex gap-x-12 mt-6 ml-12">
                     {/* Main Office */}
                     <div>
                         <h3 className="font-bold">Main Office</h3>
-                        <p>Mail: archone@mail.com</p>
-                        <p>Address: 1892 Chenoweth Drive TN</p>
-                        <p>Phone: 123-456-3451</p>
+                        <p>Mail:archone@mail.com</p>
+                        <p>Address:1892 Chenoweth Drive TN</p>
+                        <p>Phone:123-456-3451</p>
                         <a
-                            href="https://www.google.com/maps/place/1892+Chenoweth+Dr+TN"
+                            href="https://www.google.com/maps/place/Chenoweth+Ln,+La+Vergne,+TN+37086,+EE.+UU./@36.0388607,-86.54419,17z/data=!3m1!4b1!4m6!3m5!1s0x88640d7fc4f94775:0x3e6e3762ae255def!8m2!3d36.0388564!4d-86.5416151!16s%2Fg%2F1tslkg72?authuser=0&entry=ttu&g_ep=EgoyMDI0MTExMi4wIKXMDSoASAFQAw%3D%3D"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-500 hover:underline flex items-center mt-2"
@@ -50,10 +52,10 @@ export default function Contact() {
                     <div>
                         <h3 className="font-bold">Office II</h3>
                         <p>Mail: archtwo@mail.com</p>
-                        <p>Address: 3399 Wines Lane TX</p>
+                        <p>Address: 3399 Wines Lane VR</p>
                         <p>Phone: 832-123-4321</p>
                         <a
-                            href="https://www.google.com/maps/place/3399+Wines+Ln+TX"
+                            href="https://www.google.com/maps/place/Wines+Ln,+Virginia+22630,+EE.+UU./@38.943577,-78.2411456,17z/data=!3m1!4b1!4m6!3m5!1s0x89b5c6e523714431:0x167b8d43f8cd47f1!8m2!3d38.9435729!4d-78.2385707!16s%2Fg%2F1tgcbd48?authuser=0&entry=ttu&g_ep=EgoyMDI0MTExMi4wIKXMDSoASAFQAw%3D%3D"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-500 hover:underline flex items-center mt-2"
@@ -62,6 +64,9 @@ export default function Contact() {
                         </a>
                     </div>
                 </div>
+            </div>
+            <div className='ms-8 mt-12 pt-12'>
+                <img src="assets/contact/desktop/image-map.png"/>
             </div>
         </main>
     )
